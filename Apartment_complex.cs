a4407592ae8fc786c669f34f44053a0cbf4b3451
Carl <carlwskills@gmail.com>
2022-05-03 19:49:38 +0200

    teest 1

--- Apartment_complex.cs
+++ Apartment_complex.cs
@@ -3,9 +3,23 @@ namespace Apartment_application
 {
     class Apartment_complex
         {
-            public string color = "red";
+            public string[] standard = {"door", "window", "bathroom", "kitchen"};
+            public string[] uniqe= {"living_room", "sauna", "tumbler lock", "balcony"};
+            public string[] additional = {"kitchenisland", "tiles", "wet room carpet"};
             public string[] sizes = {"one_room", "two_room", "three_room", "four_room"};
-
+            public string number_of_windows;
+            class one_room {
+                string number_of_windows = 1;
+            }
+            class two_room {
+                string number_of_windows = 2;
+            }
+            class three_room {
+                string number_of_windows = 4;
+            }
+            class four_room {
+                string number_of_windows = 6;
+            }
 
         }
 }
