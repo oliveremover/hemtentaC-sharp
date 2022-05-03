0b1b2567bad890d3a307f6fcdf4ba9553abcd273
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-03 15:17:11 +0200

    Staring new project, question one

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Apartment_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
