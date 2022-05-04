ea7ca391b2c8b6ae93d7d6d94b18dcefef1f60c2
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-04 12:15:50 +0200

    A beginning

--- Apartment_complex.cs
+++ Apartment_complex.cs
@@ -6,20 +6,7 @@ namespace Apartment_application
             public string[] standard = {"door", "window", "bathroom", "kitchen"};
             public string[] uniqe= {"living_room", "sauna", "tumbler lock", "balcony"};
             public string[] additional = {"kitchenisland", "tiles", "wet room carpet"};
-            public string[] sizes = {"one_room", "two_room", "three_room", "four_room"};
-            public string number_of_windows;
-            class one_room {
-                string number_of_windows = 1;
-            }
-            class two_room {
-                string number_of_windows = 2;
-            }
-            class three_room {
-                string number_of_windows = 4;
-            }
-            class four_room {
-                string number_of_windows = 6;
-            }
+            public string[] sizes = {"1 room", " 2 room", "3 room", "4 room"};
 
         }
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,51 @@ namespace Apartment_application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Apartment_complex myObj = new Apartment_complex();
-            Console.WriteLine(myObj.color);
-            Console.WriteLine(string.Join(" ", myObj.sizes));
+            Choices obj1 = new Choices();
+            obj1.Start();
+
+
         }
+    }
+    public class Choices
+    {
+        Apartment_complex obj = new Apartment_complex();
+
+        
+        public void Start(){
+            Console.WriteLine("Choose your destiny!");
+            Choose_sizes();
+            string size_choosen = Console.ReadLine("");
+            if (size_choosen == "1"){
+
+            }
+            else if (size_choosen == "2"){
+
+            }
+            else if (size_choosen == "3"){
+                
+            }
+            else if (size_choosen == "4"){
+                
+            }
+            else {
+                Error();
+            }
+        } 
+        void Choose_sizes(){
+            Console.WriteLine("Begin by choosing size!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Sizes avalible: (ony number)");
+            Console.WriteLine(string.Join(" ", obj.sizes)); 
+            
+        }
+        void Bark(){
+            Console.WriteLine(obj.sizes);   
+        } 
+        void Error(){
+            Console.WriteLine("Input not acceptable");
+            Start();
+        }
+    
     }
 }
