6b41d1ce88586be984327a34da2e19efae94626e
Carl <carlwskills@gmail.com>
2022-05-04 18:12:43 +0200

    A bit there

--- Program.cs
+++ Program.cs
@@ -20,32 +20,57 @@ namespace Apartment_application
         public void Start(){
             Console.WriteLine("Choose your destiny!");
             Choose_sizes();
-            string size_choosen = Console.ReadLine("");
+            string size_choosen = Console.ReadLine();
+            Console.WriteLine("What standard with the size:");
             if (size_choosen == "1"){
-
+                One();
             }
             else if (size_choosen == "2"){
-
+                Two();
             }
             else if (size_choosen == "3"){
-                
+                Three();
             }
             else if (size_choosen == "4"){
-                
+                Four();
             }
             else {
                 Error();
             }
         } 
+        
         void Choose_sizes(){
             Console.WriteLine("Begin by choosing size!");
             Console.WriteLine(" ");
             Console.WriteLine("Sizes avalible: (ony number)");
-            Console.WriteLine(string.Join(" ", obj.sizes)); 
+            Console.WriteLine(string.Join(" : ", obj.sizes)); 
             
         }
-        void Bark(){
-            Console.WriteLine(obj.sizes);   
+        void One(){
+            obj.standard[1] = "1 window";
+            Console.WriteLine("The standard:");
+            Console.WriteLine(string.Join(" : ", obj.standard));   
+        }
+        void Two(){
+            obj.standard[1] = "2 window";
+            Console.WriteLine("The standard:");
+            Console.WriteLine(string.Join(" : ", obj.standard));
+            Console.WriteLine("Uniqe:");
+            Console.WriteLine(obj.standard[0]);
+        }
+        void Three(){
+            obj.standard[1] = "4 window";
+            Console.WriteLine("The standard:");
+            Console.WriteLine(string.Join(" : ", obj.standard));   
+            Console.WriteLine("Uniqe:");
+            Console.WriteLine(obj.standard[0]," : ", obj.standard[2]);
+        }
+        void Four(){
+            obj.standard[1] = "6 window";
+            Console.WriteLine("The standard:");
+            Console.WriteLine(string.Join(" : ", obj.standard));   
+            Console.WriteLine("Uniqe:");
+            Console.WriteLine(string.Join(" : ", obj.uniqe));  
         } 
         void Error(){
             Console.WriteLine("Input not acceptable");
