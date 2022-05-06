ea93afe77eeb99c376a8e665f5be9c69ded3820c
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-06 17:17:10 +0200

    updates

--- Apartment_complex.cs
+++ Apartment_complex.cs
@@ -5,17 +5,13 @@ namespace Apartment_application
         {
             public string[] standard = {"door", "window", "bathroom", "kitchen"};
             public string[] uniqe= {"living_room", "sauna", "tumbler lock", "balcony"};
-            public string[] additional = {"kitchenisland", "tiles", "wet room carpet"};
+            public string[] additional = {"kitchenisland", "tiles", "wet_room_carpet"};
             public string[] sizes = {"1 room", " 2 room", "3 room", "4 room"};
-            public string[] kitchen_island_type = {"Granite Black", " Granite White", "Laminate Black", "Laminate White"};
+            public string[] kitchen_island_type = {"Granite_Black", " Granite_White", "Laminate_Black", "Laminate_White"};
 
             public Dictionary<string, string> viewer_have_choosen =
                 new Dictionary<string, string>();
-            string standard_string = string.Join(" : ", standard);
-            public void Run(string size_chosen){
-                viewer_have_choosen.Add("Size", size_chosen);
-                viewer_have_choosen.Add("Standard", standard_string);
-            }
+
 
  
         }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ namespace Apartment_application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Choices obj1 = new Choices();
             obj1.Start();
 
@@ -21,65 +20,79 @@ namespace Apartment_application
             Console.WriteLine("Choose your destiny!");
             Choose_sizes();
             string size_choosen = Console.ReadLine();
-            obj.Run(size_choosen);
             Console.WriteLine("What comes with the apartment");
             if (size_choosen == "1"){
                 One();
-                Choose_additional();
+                continue;
             }
             else if (size_choosen == "2"){
                 Two();
-                Choose_additional();
+                continue;
             }
             else if (size_choosen == "3"){
                 Three();
-                Choose_additional();
+                continue;
             }
             else if (size_choosen == "4"){
                 Four();
-                Choose_additional();
+                continue;
             }
             else {
                 Error();
             }
+            Choose_additional();
+
+            Print_dict();
         } 
         
         void Choose_sizes(){
+            Console.WriteLine(" ");
             Console.WriteLine("Begin by choosing size!");
             Console.WriteLine(" ");
             Console.WriteLine("Sizes avalible: (ony number)");
             Console.WriteLine(string.Join(" : ", obj.sizes)); 
-            
+            for (int i = 0; i < obj.standard.Length; i++){
+                obj.viewer_have_choosen.Add(obj.standard[i], "True");
+            }
         }
         void Choose_additional(){
+            Console.WriteLine(" ");
             Console.WriteLine("Add extra:");
             Console.WriteLine("Extra avalible: (name, if more use space between)");
-            Console.WriteLine(string.Join(" : ", obj.additional)); 
-            string additional_choosen = Console.ReadLine();
-            string[] words = additional_choosen.Split(" ");
-            for( int i=0; i<words.Length; i++)
-                {
-                    if (words[i] == obj.additional[i]){
-                        obj.viewer_have_choosen.Add(words[i], "True");
+            Console.WriteLine(string.Join(" : ", obj.additional));
+            string split;
+            split = Console.ReadLine();
+            List<string> pre_list = new List<string>();
+            pre_list = split.Split(',').ToList();
+            foreach(var l in pre_list)
+            {
+                for (int i = 0; i < obj.additional.Length; i++){
+                    if (l == obj.additional[i]){
+                        obj.viewer_have_choosen.Add(obj.additional[i], "True");
                     }
                 }
+            }
+        }
 
-            
+        void Choose_kitchen_island(){
+
         }
         void One(){
+            Console.WriteLine(" ");
             obj.standard[1] = "1 window";
             Console.WriteLine("The standard:");
             Console.WriteLine(string.Join(" : ", obj.standard));   
         }
         void Two(){
+            Console.WriteLine(" ");
             obj.standard[1] = "2 window";
             Console.WriteLine("The standard:");
             Console.WriteLine(string.Join(" : ", obj.standard));
             Console.WriteLine("Uniqe:");
-            Console.WriteLine(obj.standard[0]);
-            obj.viewer_have_choosen.Add(obj.standard[0], "True");
+            Console.WriteLine(obj.uniqe[0]);
         }
         void Three(){
+            Console.WriteLine(" ");
             obj.standard[1] = "4 window";
             Console.WriteLine("The standard:");
             Console.WriteLine(string.Join(" : ", obj.standard));   
@@ -89,6 +102,7 @@ namespace Apartment_application
             obj.viewer_have_choosen.Add(obj.uniqe[2], "True");
         }
         void Four(){
+            Console.WriteLine(" ");
             obj.standard[1] = "6 window";
             Console.WriteLine("The standard:");
             Console.WriteLine(string.Join(" : ", obj.standard));   
@@ -99,8 +113,17 @@ namespace Apartment_application
             }
         } 
         void Error(){
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
             Console.WriteLine("Input not acceptable");
             Start();
+        }
+        void Print_dict(){
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is what is included");
+            foreach (KeyValuePair<string, string> kv in obj.viewer_have_choosen)
+                Console.WriteLine(kv.Key.ToString());
         }
     
     }
