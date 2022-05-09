33f8a655d6e5276cb69ad47fc59d26af38446236
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-09 10:19:50 +0200

    Uppgift 2

---
+++ Uppgift_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Cinema_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DÖDD");
+
+        }
+    }
+}
