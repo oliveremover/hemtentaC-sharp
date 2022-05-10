d71e8d81231f170910bdbd22ed6eafc1885f0c21
oliveremover <carl.bylund@edu.vellinge.se>
2022-05-10 17:49:06 +0200

    Start of Uppgift_2

--- Uppgift_2/Program.cs
+++ Uppgift_2/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 namespace Cinema_application
 {
-    class Program
+    public class Booking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DÖDD");
+
+
+            Movies movie1 = new Movies("The Unbearable Weight of Massive Talent",
+                                        "Unfulfilled and facing financial ruin, actor Nick Cage accepts a $1 million offer to attend a wealthy fan's birthday party. Things take a wildly unexpected turn when a CIA operative recruits Cage for an unusual mission. Taking on the role of a lifetime, he soon finds himself channeling his most iconic and beloved characters to save himself and his loved ones.",
+                                        106);
+            Movies movie2 = new Movies("Fantastiska vidunder: Dumbledores hemligheter","Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.",143);
+
+            movie1.create_seat_map();
+            movie2.create_seat_map();
+
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Welcome to the Cinema!");
+            Console.WriteLine("These are the movies we have on display:");
+            // Create an instance of WorkItem by using the constructor in the
+            // base class that takes three arguments.
+
+            Console.WriteLine(movie1.ToString());
+            Console.WriteLine(movie2.ToString());
+
+            Console.WriteLine("Please choose the movie you wanna watch:");
+            string movie_to_watch = Console.ReadLine();
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Number of tickets: ");
+            string number_of_tickets = Console.ReadLine();
+            if (movie_to_watch == "1"){
+                movie1.show_seat_map();
+            }
+            else if (movie_to_watch == "2"){
+                movie2.show_seat_map();
+            }
+            
+            
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("What seats do you want: ");
+            
+            // Create an instance of ChangeRequest by using the constructor in
+            // the derived class that takes four arguments.
+            //Book_seats book = new Book_seats(movie_to_watch, row, seat, 120);
+
+            // Use the ToString method defined in WorkItem.
 
         }
+
     }
 }
---
+++ Uppgift_2/booking.cs
@@ -0,0 +1,145 @@
+using System;
+namespace Cinema_application
+{
+public class Movies
+{
+    // Static field currentID stores the job ID of the last WorkItem that
+    // has been created.
+    private static int currentID;
+
+    protected List<string> seats_map = new List<string>();
+
+    protected List<string> seats_taken = new List<string>();
+    //Properties.
+    protected int ID { get; set; }
+    protected string Title { get; set; }
+    protected string Description { get; set; }
+    protected int MovieLength { get; set; }
+    protected string Row { get; set; }
+    protected int Seat{ get; set; }
+
+    // Default constructor. If a derived class does not invoke a base-
+    // class constructor explicitly, the default constructor is called
+    // implicitly.
+    public Movies()
+    {
+        ID = 0;
+        Title = "Default title";
+        Description = "Default description.";
+        MovieLength = 0;
+    }
+
+    // Instance constructor that has three parameters.
+    public Movies(string title, string desc, int movielen)
+    {
+        this.ID = GetNextID();
+        this.Title = title;
+        this.Description = desc;
+        this.MovieLength = movielen;
+    }
+
+    // Static constructor to initialize the static member, currentID. This
+    // constructor is called one time, automatically, before any instance
+    // of WorkItem or ChangeRequest is created, or currentID is referenced.
+    static Movies() => currentID = 0;
+
+    // currentID is a static field. It is incremented each time a new
+    // instance of WorkItem is created.
+    protected int GetNextID() => ++currentID;
+
+    // Method Update enables you to update the title and job length of an
+    // existing WorkItem object.
+    public void Update_seat(string row, int seat)
+    {
+        this.Row = row;
+        this.Seat = seat;
+
+        object value = row + " : " + seat;
+        for(int i = 0; i < seats_map.Count; i++){
+            if(seats_map[i] == value.ToString()){
+                seats_map[i] = "Occupied";
+            }
+        }
+    }
+
+    public void create_seat_map(){
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 1 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 2 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 3 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 4 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 5 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 6 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 7 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 8 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 9 : " + i;
+            seats_map.Add(value.ToString());
+        }
+        for(int i = 0; i <= 15; i++){
+            object value = "Row 10 : " + i;
+            seats_map.Add(value.ToString());
+        }
+    }
+    public void show_seat_map(){
+        for(int i = 0; i < seats_map.Count;i++){
+            Console.WriteLine(seats_map[i]);
+        }
+    }
+
+    // Virtual method override of the ToString method that is inherited
+    // from System.Object.
+    public override string ToString() =>
+        $"{this.ID} - {this.Title}";
+}
+
+// ChangeRequest derives from WorkItem and adds a property (originalItemID)
+// and two constructors.
+public class Book_seats : Movies
+{
+    // Constructors. Because neither constructor calls a base-class
+    // constructor explicitly, the default constructor in the base class
+    // is called implicitly. The base class must contain a default
+    // constructor.
+
+    // Default constructor for the derived class.
+    public Book_seats() { }
+
+    // Instance constructor that has four parameters.
+    public Book_seats(string row, int seat,
+                         int originalID)
+    {
+        // The following properties and the GetNexID method are inherited
+        // from WorkItem.
+        this.ID = GetNextID();
+        this.Row = row;
+        this.Seat = seat;
+
+        // Property originalItemId is a member of ChangeRequest, but not
+        // of WorkItem.
+    }
+}
+}
